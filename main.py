34e43523f05784254b4f7808a0ed80723f295054
Lukas Andersson <j.lukas.andersson@gmail.com>
2025-04-10 13:44:01 +0200

    added functionality to select whether to run the data extraction, output generation, or both.

--- main.py
+++ main.py
@@ -6,14 +6,30 @@ DATABASE_LOCATION = "sqlite:///my_tracks.sqlite"
 
 if __name__ == "__main__":
 
-    # run spotify extraction and return a pandas dataframe containing
-    spotify_extraction.run(DATABASE_LOCATION)
+    while True:
+        ans = input("Do you want to update the database with new data? Answer with Yes/y or No/n: ").upper()
 
-    # perform further metadata extractions
-    acousticbrainz_extraction.run(DATABASE_LOCATION)
+        if ans in ["YES", "Y", "NO", "N"]:
+            break
+        else:
+            print("Invalid input. Answer with yes/y or no/n.")
 
-    # combine into large table containing all raw data.
-    sql_operations.run(DATABASE_LOCATION)
+    if ans in ["YES", "Y"]:
+        # run spotify extraction
+        spotify_extraction.run(DATABASE_LOCATION)
+        # perform further metadata extractions
+        acousticbrainz_extraction.run(DATABASE_LOCATION)
+        # combine into large table containing all raw data.
+        sql_operations.run(DATABASE_LOCATION)
 
+    while True:
+        ans2 = input("Do you want to create excel-tables using the data available in the database? Answer with Yes/y or No/n: ").upper()
 
-    sql_operations.create_brightness_and_danceability_sheet(DATABASE_LOCATION, "./exports")
+        if ans2 in ["YES", "Y", "NO", "N"]:
+            break
+        else:
+            print("Invalid input. Answer with yes/y or no/n.")
+
+    if ans2 in ["YES", "Y"]:
+        sql_operations.create_brightness_and_danceability_hourly_sheet(DATABASE_LOCATION, "./exports")
+        sql_operations.create_spotify_hourly_sheet(DATABASE_LOCATION, "./exports")
